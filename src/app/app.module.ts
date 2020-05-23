f12f6b89cdefcffdcaa5d34714292a1ae427aa80
AndreFontaine <andres.gonzalez.ud@gmail.com>
2020-05-23 14:15:10 +0100

    extract application layout in components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CardComponent } from './components/card.component';
+import { HeaderComponent } from './components/header.component';
+import { CardListComponent } from './components/card-list.component';
+import { CardSmallListComponent } from './components/card-small-list.component';
+import { CardSmallComponent } from './components/card-small.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardListComponent,
+    CardComponent,
+    CardSmallListComponent,
+    CardSmallComponent,
+    HeaderComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
