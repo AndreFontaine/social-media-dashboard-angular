0dee929358cd6575a3a5575d5c9c4367d8f66ac9
AndreFontaine <andres.gonzalez.ud@gmail.com>
2020-05-23 14:02:19 +0100

    add card-small-list component

---
+++ src/app/components/card-small-list.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-small-list',
+  templateUrl: './card-small-list.component.html',
+  styleUrls: ['./card-small-list.component.css'],
+})
+export class CardSmallListComponent {}
