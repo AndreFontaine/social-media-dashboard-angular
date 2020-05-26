4e6e35821fa292e8c51a50e801256d9ab9bb260e
AndreFontaine <andres.gonzalez.ud@gmail.com>
2020-05-26 19:14:08 +0100

    implemets the switch behavior

--- src/app/components/header.component.ts
+++ src/app/components/header.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   title = 'Social Media Dashboard';
+  darkMode = true;
+
+  constructor() {
+    const mq = window.matchMedia('(prefers-color-scheme: dark)');
+    if (mq) {
+      this.toggleDark();
+    }
+  }
+
+  darkModeActive(selected) {
+    if (selected) {
+      this.toggleDark();
+    } else {
+      this.toggleLight();
+    }
+  }
+
+  toggleDark() {
+    document.body.classList.remove('is-light-mode');
+    document.body.classList.add('is-dark-mode');
+  }
+
+  toggleLight() {
+    document.body.classList.remove('is-dark-mode');
+    document.body.classList.add('is-light-mode');
+  }
 }
--- src/app/components/switch.component.ts
+++ src/app/components/switch.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter, Input } from '@angular/core';
 
 @Component({
   selector: 'app-switch-mode',
   templateUrl: './switch.component.html',
   styleUrls: ['./switch.component.css'],
 })
-export class SwitchComponent {}
+export class SwitchComponent {
+  @Input() darkModeActive: boolean;
+  @Output() schemeChanged: EventEmitter<boolean> = new EventEmitter(true);
+
+  public changeDarkMode(selected: boolean) {
+    this.schemeChanged.emit(selected);
+  }
+}
